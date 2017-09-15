b1e2b860db65262c807439449c255f5c13185510
Roy Sprowl <roys@jetstreamsoftware.com>
2017-09-14 18:06:59 -0700

    initial checkin

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
+# azure-iot-pal-arduino
 
-# Contributing
+
+
+
+
+### Contributing
 
 This project welcomes contributions and suggestions.  Most contributions require you to agree to a
 Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us
---
+++ build_all/arduino_cc/base-libraries/AzureIoTHub/src/AzureIoTHub.h
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef AZUREIOTHUB_H
+#define AZUREIOTHUB_H
+
+#include "AzureIoTUtility.h"
+
+#include "sdk/serializer.h"
+
+#include "sdk/iothub_client_ll.h"
+#include "sdk/iothub_message.h"
+#include "sdk/iothubtransporthttp.h"
+#include "sdk/iothubtransportmqtt.h"
+
+#define AzureIoTHubVersion "1.0.40"
+#endif
