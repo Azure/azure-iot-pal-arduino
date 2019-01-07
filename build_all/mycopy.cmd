e1ce7404bf5d6a089f8b08d9f0717affe6ec74f5
Mark Radbourne <markrad@msn.com>
2019-01-07 13:29:43 -0800

    Remove test files

--- build_all/mycopy.cmd
+++
@@ -1,7 +0,0 @@
-set DIR=%USERPROFILE%\Documents\Arduino
-set NEWLOC=%DIR%\libraries_staging
-set OLDLOC=%DIR%\libraries
-
-for /d %%G in ("%OLDLOC%\AzureIoT*") do rd /s /q "%%~G"
-
-xcopy %NEWLOC%\* %OLDLOC% /E
