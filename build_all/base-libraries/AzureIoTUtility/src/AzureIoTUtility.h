9431e463157ee29a12af141e6784427ff811fdcd
jbobotek <jeremy.bobotek@gmail.com>
2019-10-28 13:52:03 -0700

    Update AzureIoTUtility.h

--- build_all/base-libraries/AzureIoTUtility/src/AzureIoTUtility.h
+++ build_all/base-libraries/AzureIoTUtility/src/AzureIoTUtility.h
@@ -8,6 +8,6 @@
 #include "azure_c_shared_utility/lock.h"
 #include "azure_c_shared_utility/threadapi.h"
 
-#define AzureIoTUtilityVersion "2.0.0"
+#define AzureIoTUtilityVersion "1.1.12"
 
 #endif //AZUREIOTUTILITY_H
