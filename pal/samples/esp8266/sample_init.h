a4a3933682f2bcc16546b84f6022466344da2b1f
jbobotek <jeremy.bobotek@gmail.com>
2020-01-15 16:17:43 -0800

    Update sample_init.h

--- pal/samples/esp8266/sample_init.h
+++ pal/samples/esp8266/sample_init.h
@@ -7,11 +7,11 @@
   #define sample_init esp8266_sample_init
   #define is_esp_board
   void esp8266_sample_init(const char* ssid, const char* password);
-#endif
+#endif // ARDUINO_ARCH_ESP8266
 #if defined(ARDUINO_ARCH_ESP32)
   #define sample_init esp32_sample_init
   #define is_esp_board
   void esp32_sample_init(const char* ssid, const char* password);
-#endif
+#endif // ARDUINO_ARCH_ESP32
 
 #endif // SAMPLE_INIT_H
